5a11582ba73a45deb9a9824e52fc8eb933d4e338
QC3284 <133119288+QC3284@users.noreply.github.com>
2024-04-05 22:06:25 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,9 +17,9 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # 下载新主题
-# rm -rf package/lean/luci-theme-argon
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
-#git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge
+rm -rf package/lean/luci-theme-argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon
+git clone -b 18.06 https://github.com/r1172464137/luci-theme-edge.git package/luci-theme-edge
 # git clone https://github.com/jerrykuku/luci-app-vssr.git package/lean/luci-app-vssr
 # git clone https://github.com/jerrykuku/lua-maxminddb.git package/lean/lua-maxminddb
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
